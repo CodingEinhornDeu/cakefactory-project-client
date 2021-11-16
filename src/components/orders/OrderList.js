aa3881986cace1df76e9bb1c871d327d15324c70
CodingEinhornDeu <88826323+CodingEinhornDeu@users.noreply.github.com>
2021-11-16 21:55:09 +0100

    OrderList Component

---
+++ src/components/orders/OrderList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class OrderList extends Component {
+  
+  render() {
+    return (
+      <div>
+        <div style={{ width: '60%', float: "right" }}>
+          <h1>Order List</h1>
+          {this.props.allOrderList.map(order => {
+            return (
+              <div key={order._id}>
+                <Link to={`/orders/${order._id}`}>
+                  <h3>{order._id}</h3>
+                </Link>
+                
+              </div>
+            )
+          })
+          }
+
+        </div>
+
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+export default OrderList;
